b53cbf5e9bb1d4bacfdf55b08848167b6fe043b8
Uzair Saleem <uzairsaleemdev@gmail.com>
2024-03-25 15:27:33 -0700

    Revert "debugging the error photopath"
    This reverts commit 2d9929b7c87b67a83e3b50894ceac20a944a4e25.

--- models/blog.js
+++ models/blog.js
@@ -3,7 +3,7 @@ const Schema =mongoose.Schema;
 const blogSchema= new Schema({
     title:{type: String, required: true},
     content:{type: String, required: true},
-    description:{type:String, required:true},
+    description:{type:String, required:true}
     photoPath:{type: String, required: true},
     author:{type: mongoose.SchemaTypes.ObjectId, ref: "User"}
 }
