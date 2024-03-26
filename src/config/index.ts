7ed831899888cacfbdda8ef71bb697f4b1c84103
Uzair Saleem <uzairsaleemdev@gmail.com>
2024-03-25 19:33:13 -0700

    Update added config for dotenv ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,16 +1,26 @@
 const dotenv=require("dotenv").config()
-const PORT:number = process.env.PORT;
-const MONGODB_CONNECTION_STRING:string=process.env.MONGODB_CONNECTION_STRING;
-const ACCESS_TOKEN_SECRET:string= process.env.ACCESS_TOKEN_SECRET;
-const REFRESH_TOKEN_SECRET:string= process.env.REFRESH_TOKEN_SECRET;
-const BACKEND_SERVER_PATH:number=process.env.BACKEND_SERVER_PATH;
-const API_SECRET = process.env.API_SECRET
-const API_KEY = process.env.API_KEY
-const CLOUD_NAME = process.env.CLOUD_NAME
-module.exports={
-    PORT,
-    MONGODB_CONNECTION_STRING,
-    ACCESS_TOKEN_SECRET,
-    REFRESH_TOKEN_SECRET,
-    BACKEND_SERVER_PATH,API_KEY,API_SECRET,CLOUD_NAME
+
+interface envInterface{
+    PORT:string|undefined;
+    MONGODB_CONNECTION_STRING:string|undefined;
+    ACCESS_TOKEN_SECRET:string|undefined;
+    REFRESH_TOKEN_SECRET:string|undefined;
+    BACKEND_SERVER_PATH:string|undefined;
+    API_SECRET:string|undefined;
+    API_KEY:string|undefined;
+    CLOUD_NAME:string|undefined;
 }
+
+
+const envObj: envInterface = {
+ PORT: process.env.PORT,
+ MONGODB_CONNECTION_STRING:process.env.MONGODB_CONNECTION_STRING,
+ ACCESS_TOKEN_SECRET:process.env.ACCESS_TOKEN_SECRET,
+ REFRESH_TOKEN_SECRET:process.env.REFRESH_TOKEN_SECRET,
+ BACKEND_SERVER_PATH:process.env.BACKEND_SERVER_PATH,
+ API_SECRET :process.env.API_SECRET,
+ API_KEY : process.env.API_KEY,
+ CLOUD_NAME: process.env.CLOUD_NAME,
+}
+
+module.exports=envObj;
